d4e83f9c44d81339a462f29ee613199fdaeeb90a
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2019-03-02 21:36:01 -0300

    Fix error LikeController findById

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Proyecto NodeJS
+# Creación
+yarn init -y
+# Instalación de librerias
+yarn add express
+yarn add nodemon --dev
+yarn add mongoose
+yarn add cors
+yarn add socket.io
+
 # MongoDB
 ## Crear base de datos
 use twitter-db
--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -3,11 +3,13 @@ const Tweet = require('../models/Tweet');
 async function create(req, res) {
     const tweet = await Tweet.findById(req.params.id);
 
-    tweet.set({ likes: tweet.likes + 1 });
+    if (tweet) {
+        tweet.set({ likes: tweet.likes + 1 });
 
-    await tweet.save();
+        await tweet.save();
 
-    req.io.emit('like', tweet);
+        req.io.emit('like', tweet);
+    }
 
     return res.json(tweet);
 };
